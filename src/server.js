7d376437fe69b0c084e2f52a1cb158b035605728
Erick Henrique <erickhenrique321@gmail.com>
2018-10-21 18:23:09 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+app.use(express.static(`${__dirname}/app/css`));
+app.use(express.static(`${__dirname}/app/img`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/app/index.html`);
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
